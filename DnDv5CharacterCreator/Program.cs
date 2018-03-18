8fe9ccc1d6e995fdb30be23a4dd124587dc3d91e
James Lynch <eimi_christian@hotmail.com>
2018-03-18 14:43:15 -0400

    Initial Commit

--- DnDv5CharacterCreator/Program.cs
+++ DnDv5CharacterCreator/Program.cs
@@ -16,7 +16,7 @@ namespace DnDv5CharacterCreator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
